7560a2bf6ae9ce640ef131414520651c6efe0218
Alexandre Sedenho <ale.sedenho@gmail.com>
2026-07-27 22:04:59 -0300

    Add files via upload

---
+++ aula 1/meia.php
@@ -0,0 +1,5 @@
+<?php
+$inteira = 53.90;
+$metade = $inteira / 2;
+echo " Meia: " . $metade;
+?>
---
+++ aula 1/produtos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action= 'preco.php'>
+        <label>Preço</label>
+    <input type= 'text' name= 'preco'>
+    <input type= 'submit'>
+<br>
+    <form action= 'preco.php'>
+        <label>Quantidade</label>
+    <input type= 'text' name= 'quantidade'>
+    <input type= 'submit'>
+</form>
+</body>
+</html>
